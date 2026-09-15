ff88a6b131014649fd7b82c6402fa153148eeb45
Fielding Hoffmeier <Fieldinghoffmeier@gmail.com>
2026-09-15 11:56:55 -0600

    Did assignment

---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+import random
+
+
+_ = input(f"Press enter to start. When you see \"GO{chr(33)}\" hit enter")
+
+reactionTimes = []
+
+totalPlays = 0
+maxPlays = 5
+
+while totalPlays < maxPlays:
+    time.sleep(random.uniform(2, 5))
+    start = time.monotonic()
+    _ = input(f"GO{chr(33)}")
+    reactionTime = round(time.monotonic() - start, 4) * 1000
+    if reactionTime > 0:
+        print(str(reactionTime) + " ms")
+        reactionTimes.append(reactionTime)
+        totalPlays += 1
+    else:
+        print("Try again, reaction time invalid, hit enter after you see GO")
+
+averageReactionTime = 0
+fastestReactionTime = 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+for reactionTime in reactionTimes:
+    averageReactionTime += reactionTime
+    if reactionTime < fastestReactionTime:
+        fastestReactionTime = reactionTime
+averageReactionTime /= maxPlays
+
+print(f"Finished - {round(averageReactionTime, 1)} ms average reaction time and {fastestReactionTime} ms fastest reaction time")
